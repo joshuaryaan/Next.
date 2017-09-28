8eb0f6aebaf838aafae0e7a5f59aade43467c560
Josh Warner <joshua.ryan.warner@icloud.com>
2017-09-28 21:43:02 +1300

    Possible vh fix

--- js/script.js
+++ js/script.js
@@ -32,9 +32,31 @@ $(document).ready(function () {
     //SEARCH
     $(".grid li").css( "opacity", "0" );
     
-    $(window).resize(function() {
-        $("body, .page, .menu").css({ height: window.innerHeight });
-    });
+    
+    if(navigator.platform == 'iPhone' || navigator.platform == 'iPod'){
+        // In Safari, the true version is after "Safari" 
+        if ((verOffset=nAgt.indexOf('Safari'))!=-1) {
+          // Set a variable to use later
+          var mobileSafari = 'Safari';
+        }
+    }
+
+    // If is mobile Safari set window height +60
+    if (mobileSafari == 'Safari') { 
+        // Height + 60px
+        $("body, .page .menu").css('height', window.innerHeight);
+    } else {
+        // Else use the default window height
+        $("body, .page .menu").css('height', "100" + '%');  
+    };
+    
+    
+    
+    
+    
+    
+    
+    
     
     $(document).bind('touchmove', function(e) {
         e.preventDefault();
